ce0b60143809abc7b86f6afd4222125ec3203936
akramassi <akram.20002010@gmail.com>
2020-10-20 12:52:17 +0300

    Preparation setOnNavigationItemSelectedListener

--- app/src/main/java/com/example/convertor/MainActivity.java
+++ app/src/main/java/com/example/convertor/MainActivity.java
@@ -18,6 +18,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener;
 
 import java.util.Locale;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
 
                 switch (item.getItemId())
                 {
-
 
                     case R.id.light:
                     {
@@ -70,37 +70,14 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
 
-                getSupportFragmentManager().beginTransaction().replace(R.id.cont,fragment).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.cont, Objects.requireNonNull(fragment)).commit();
                 return true;
             }
         });
 
-//        setLtr();
 
 
     }
 
-    private void setLtr() {
-        String languageToLoad = "en"; // ltr language English
-        Locale locale = new Locale(languageToLoad);
-        Locale.setDefault(locale);
-
-        Configuration config = new Configuration();
-        config.locale = locale;
-        getBaseContext().getResources().updateConfiguration(config,
-                getBaseContext().getResources().getDisplayMetrics());
-        //layout direction
-        Bidi b = new Bidi(languageToLoad, Bidi.DIRECTION_DEFAULT_LEFT_TO_RIGHT);
-        b.isLeftToRight();
-        //save current locale in SharedPreferences
-        SharedPreferences languagepref = getSharedPreferences("language", MODE_PRIVATE);
-        SharedPreferences.Editor editor = languagepref.edit();
-        editor.putString("languageToLoad", languageToLoad);
-        editor.commit();
-
-        Intent refresh = new Intent(this, MainActivity.class);
-        finish();
-        startActivity(refresh);
-    }
 
 }
